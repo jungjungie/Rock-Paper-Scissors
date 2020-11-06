d618709761c66d0d937fef33b3e362d9341a0f7f
Esther Min <estherjmin@gmail.com>
2020-11-05 21:36:43 -0500

    created function to randomly select computer's move

--- script.js
+++ script.js
@@ -2,17 +2,33 @@
 let rockCard = document.querySelector("#rockCard");
 let paperCard = document.querySelector("#paperCard");
 let scissorsCard = document.querySelector("#scissorsCard");
+let userMoveCard = document.querySelector("#userMoveCard");
 
 let userMove = "";
+let compMove = ""
 
+// Sets the user move based on the rps card clicked
 function setUserMove(event) {
     // console.log(event.target)
     // console.log(event.target.textContent)
 
-    userMove = event.target.textContent;
+    userMove = event.target.textContent.trim();
     console.log(userMove);
+
+    userMoveCard.textContent = userMove;
+
+    setCompMove();
 }
 
+// Randomly sets the computer's move
+function setCompMove() {
+    let moves = ["ROCK", "PAPER", "SCISSORS"];
+
+    compMove = moves[Math.floor(Math.random() * 3)];
+    console.log(compMove);
+}
+
+// Event listeners
 rockCard.addEventListener("click", setUserMove);
 paperCard.addEventListener("click", setUserMove);
 scissorsCard.addEventListener("click", setUserMove);
